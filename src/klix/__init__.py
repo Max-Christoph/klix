d1d6968b3c775e38a4a9aaa604873164965bb7fb
Max-Christoph Hähnel <m-c-haehnel@gmx.de>
2026-09-22 16:10:30 +0200

    v0.1.2: Publish-Workflow mit PYPI_TOKEN (End-to-End-Test)

--- src/klix/__init__.py
+++ src/klix/__init__.py
@@ -3,7 +3,7 @@
 from klix.engine import DecisionEngine, DecisionResult
 from klix.heads import BaseHead, Choice, Flag, Score
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = [
     "DecisionEngine",

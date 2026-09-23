a8e2453153d9d2d501d93d50ac706c59279f094e
Max-Christoph Hähnel <m-c-haehnel@gmx.de>
2026-09-23 16:48:58 +0200

    bump to 0.7.1

--- src/klix/__init__.py
+++ src/klix/__init__.py
@@ -4,7 +4,7 @@ from klix.engine import DecisionEngine, DecisionResult
 from klix.heads import BaseHead, Choice, Flag, Score
 from klix.rules import Rule
 
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 
 __all__ = [
     "DecisionEngine",
